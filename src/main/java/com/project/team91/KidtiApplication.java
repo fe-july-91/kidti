33c13163301eb7666d42894e69f65647a3d1f174
Yana Stepanova <yanna.stepanova@gmail.com>
2024-10-15 00:03:15 +0300

    added uml-model and checkstyle

---
+++ src/main/java/com/project/team91/KidtiApplication.java
@@ -0,0 +1,13 @@
+package com.project.team91;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidtiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidtiApplication.class, args);
+	}
+
+}
