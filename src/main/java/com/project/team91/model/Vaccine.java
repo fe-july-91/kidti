5e158eb45e0eb080e2a2f97d813719b9e770ddbe
Yana Stepanova <yanna.stepanova@gmail.com>
2024-10-16 18:16:02 +0300

    added all entity-classes + yaml-files of creating and inserting

--- src/main/java/com/project/team91/model/Vaccine.java
+++ src/main/java/com/project/team91/model/Vaccine.java
@@ -2,16 +2,14 @@ package com.project.team91.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import java.time.LocalDate;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import java.time.LocalDate;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
@@ -20,26 +18,23 @@ import org.hibernate.annotations.SQLRestriction;
 @Getter
 @Setter
 @Entity
-@SQLDelete(sql = "UPDATE childs SET is_deleted = TRUE WHERE id = ?")
+@SQLDelete(sql = "UPDATE vaccines SET is_deleted = TRUE WHERE id = ?")
 @SQLRestriction("is_deleted = FALSE")
-@Table(name = "childs")
-public class Child {
+@Table(name = "vaccines")
+public class Vaccine {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false)
-    private String name;
-    @Column(nullable = false)
-    private String surname;
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
+    @JoinColumn(name = "type_id", nullable = false)
+    private Type type;
     @Column(nullable = false)
-    private LocalDate birth;
-    @Enumerated(EnumType.STRING)
+    private byte orderNumber;
     @Column(nullable = false)
-    private Gender gender;
-    private String image;
+    private LocalDate date;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "child_id", nullable = false)
+    private Child child;
     @Column(name = "is_deleted", nullable = false)
     private boolean isDeleted;
 }
